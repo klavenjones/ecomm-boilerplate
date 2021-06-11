a7b75203bc1f418018cebe3c23914ee0170fbb6b
Klaven Jones <klavensjones@gmail.com>
2021-06-11 10:07:10 -0700

    Initial Commit

---
+++ component/product.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+import { products } from '../lib/product'
+
+function Product({ product: { title, description, image, alt } }) {
+  return (
+    <li className={styles.card}>
+      <a href='https://nextjs.org/docs'>
+        <img src={image} alt={alt} />
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </a>
+    </li>
+  )
+}
+
+function ProductList() {
+  return products.map((product) => (
+    <Product key={product.id} product={product} />
+  ))
+}
+
+export { ProductList }
